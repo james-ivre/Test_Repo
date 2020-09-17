7e5be66553e13071353cfe89d63e6c4828dd49eb
james-ivre <james@immersivevreducation.com>
2020-09-17 17:10:10 +0100

    New package

--- ENGAGE_SceneCreator/Scripts/Engine/GUIGamesSortingGamePickup.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GUIGamesSortingGamePickup : MonoBehaviour {
-	public GameObject showObject;
-	public string sortingGame;
-	public string[] sortingGameItems;
-}
